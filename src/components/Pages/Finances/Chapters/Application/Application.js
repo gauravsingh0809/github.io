b363edf7d41e728f813c210a783d3bdf6c114650
gauravsingh0809 <gaurav080992@gmail.com>
2022-02-25 17:06:05 +0530

    Application and resources pages

--- src/components/Pages/Finances/Chapters/Application/Application.js
+++ src/components/Pages/Finances/Chapters/Application/Application.js
@@ -19,13 +19,97 @@ const  Application = () => {
                     <Navbar />
                     <div className="contentwrapper" id="home">
                         <FloatingActionButtons back="/finances" forward="/finances/major-sources" />
-                        <Title>{ctx.chapterTwo.Profile.Para4}</Title>
-                        <Para>
-                            {ctx.chapterTwo.Profile.Para2}
+                        <Title>{ctx.chapterTwo.Profile.Para67}</Title>
+                        {["Para68","Para69","Para70","Para71"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                            {ctx.chapterTwo.Profile.Para72}
                         </Para>
                         <Para>
-                            {ctx.chapterTwo.Profile.Para3}
+                            {ctx.chapterTwo.Profile.Para73}
                         </Para>
+                        {["Para74","Para75","Para76","Para77","Para78"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                            {ctx.chapterTwo.Profile.Para79}
+                        </Para>
+                        {["Para80","Para81","Para82",].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para83","Para84","Para85",].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para86","Para87","Para88","Para89"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para90","Para91","Para92","Para93","Para94","Para95","Para96","Para97","Para98","Para99","Para100","Para101","Para102","Para103","Para104","Para105","Para106","Para107","Para108","Para109"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                            {ctx.chapterTwo.Profile.Para110}
+                        </Para>
+                        {["Para111","Para112","Para113","Para114","Para115","Para116"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+
+                            {["Para117","Para118","Para119"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para120","Para121","Para122","Para123","Para124"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                            {ctx.chapterTwo.Profile.Para125}
+                        </Para>
+                        {["Para126","Para127","Para128","Para129","Para130","Para131","Para132","Para133","Para134","Para135","Para136","Para137",].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para138","Para139","Para140","Para141"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                        {["Para142","Para143","Para144","Para145","Para146","Para147","Para148"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            </Para>
+                            {["Para149","Para150","Para151","Para152","Para153","Para154","Para155","Para156","Para157"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            <Para>
+                            {ctx.chapterTwo.first.para1h1}
+                        </Para>
+                        <Para>
+                            {ctx.chapterTwo.first.para1}
+                        </Para>
+                        <Para>
+                        {["para2","para3"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.first[item]}
+                            </Para> )}
+                            </Para>
+                            <Para>
+                        {["para5","para6","para7","para8","para9","para10","para11","para12","para13","para14","para15","para16","para17","para18","para18","para19","","Para20","para21","para22","para23","para24","para25","para26","para27","para28","para29","para30","para31","para32","para33"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.first[item]}
+                            </Para> )}
+                            </Para>
+
+
 
                     </div>
                 </div>
--- src/components/Pages/Finances/Chapters/Resources/Resources.js
+++ src/components/Pages/Finances/Chapters/Resources/Resources.js
@@ -27,6 +27,46 @@ const  Resources= () => {
                             <Para key={ind / 10}>
                                 {ctx.chapterTwo.Profile[item]}
                             </Para> )}
+                           {["Para11", "Para12","Para13","Para14", "Para15","Para16","Para17","Para18","Para19","Para21"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para22", "Para23","Para24","Para25"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+
+                            {["Para26", "Para27","Para28","Para29", "Para30","Para31","Para32"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+
+                            {["Para32", "Para33","Para34", "Para35","Para36","Para37","Para38","Para39"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+
+                            {["Para40", "Para41","Para42", "Para43"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para44", "Para45","Para46","Para47", "Para48","Para49","Para50","Para51","Para52","Para53","Para54"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+
+                            {["Para55","Para56","Para57","Para58","Para59","Para60"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para61","Para62","Para63","Para64"].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
+                            {["Para65","Para66",].map((item, ind) =>
+                            <Para key={ind / 10}>
+                                {ctx.chapterTwo.Profile[item]}
+                            </Para> )}
 
                     </div>
                 </div>
